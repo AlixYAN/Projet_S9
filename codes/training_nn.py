ad54eadac6d4a85918aab73d501ff16a3b3cc930
pedroc5 <41348241+pedroc5@users.noreply.github.com>
2019-10-21 10:17:11 +0200

    Update training_nn.py

--- codes/training_nn.py
+++ codes/training_nn.py
@@ -6,14 +6,6 @@ Created on Mon Oct 14 10:03:55 2019
 @author: Pedro, Rodrigo, Alix
 """
 
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 14 09:09:46 2019
-
-@author: Pedro
-"""
-
 import librosa
 import pandas as pd
 import numpy as np
