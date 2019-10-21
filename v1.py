a0dbec4f1973f9a7064950605e56262e3541e4e1
RBPinheiro13 <47257758+RBPinheiro13@users.noreply.github.com>
2019-10-21 09:26:23 +0200

    Add files via upload
    Updated

--- v1.py
+++ v1.py
@@ -1,11 +1,9 @@
-# IMPORT LIBRARIES
-
 # feature extractoring and preprocessing data
 import librosa
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-# matplotlib inline
+#matplotlib inline
 import os
 from PIL import Image
 import pathlib
@@ -21,3 +19,50 @@ import keras
 import warnings
 warnings.filterwarnings('ignore')
 
+cmap = plt.get_cmap('plasma')
+
+plt.figure(figsize=(10,10))
+genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
+for g in genres:
+    pathlib.Path(f'img_data/{g}').mkdir(parents=True, exist_ok=True)
+    for filename in os.listdir(f'./MIR/genres/{g}'):
+        songname = f'./MIR/genres/{g}/{filename}'
+        y, sr = librosa.load(songname, mono=True, duration=5)
+        plt.specgram(y, NFFT=2048, Fs=2, Fc=0, noverlap=128, cmap=cmap, sides='default', mode='default', scale='dB');
+        plt.axis('off');
+        plt.savefig(f'img_data/{g}/{filename[:-3].replace(".", "")}.png')
+        plt.clf()
+
+        
+#header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
+#for i in range(1, 21):
+#    header += f' mfcc{i}'
+#header += ' label'
+#header = header.split()
+#
+#
+#
+#file = open('data.csv', 'w', newline='')
+#with file:
+#    writer = csv.writer(file)
+#    writer.writerow(header)
+#genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
+#for g in genres:
+#    for filename in os.listdir(f'./MIR/genres/{g}'):
+#        songname = f'./MIR/genres/{g}/{filename}'
+#        y, sr = librosa.load(songname, mono=True, duration=30)
+#        chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+#        spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr) #Spectral centroid, first parameter extracted.
+#        spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+#        rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+#        zcr = librosa.feature.zero_crossing_rate(y)
+#        rmse = librosa.feature.rms(y=y)
+#        mfcc = librosa.feature.mfcc(y=y, sr=sr)
+#        to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'
+#        for e in mfcc:
+#            to_append += f' {np.mean(e)}'
+#        to_append += f' {g}'
+#        file = open('data.csv', 'a', newline='')
+#        with file:
+#            writer = csv.writer(file)
+#            writer.writerow(to_append.split())
