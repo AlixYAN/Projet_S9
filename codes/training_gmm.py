9f68ce9cd8192c2ec8e266f469a6fc1aa07a680d
Pedro Coutinho <pedro_coutinho01@hotmail.com>
2019-11-25 10:54:48 +0100

    Mean using several seeds

--- codes/training_gmm.py
+++ codes/training_gmm.py
@@ -21,18 +21,11 @@ warnings.filterwarnings('ignore')
 
 #cmap = plt.get_cmap('inferno')
 
-data = pd.read_csv('../features_csv/data_all.csv')
+data = pd.read_csv('../features_csv/data_reggae_hiphop.csv')
 #data = pd.read_csv('../features_csv/data_reagge_hiphop.csv')
            
 labels = np.unique(data['label'])
  
-#data.shape
-
-#Initialization of seeds to reproduce results
-seed_init = 1
-np.random.seed(seed_init)
-random.seed(seed_init)
-
 # Dropping unneccesary columns
 data = data.drop(['filename'],axis=1)
 
@@ -40,32 +33,31 @@ genre_list = data.iloc[:, -1]
 encoder = LabelEncoder()
 y = encoder.fit_transform(genre_list)
 
-scaler = StandardScaler()
-X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype = float))
+conf_mat_total = []
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+for seed_init in range(100):
 
-#x_val = X_train[:200]
-#partial_x_train = X_train[200:]
-#
-#y_val = y_train[:200]
-#partial_y_train = y_train[200:]
+    #Initialization of seeds to reproduce results
+    np.random.seed(seed_init)
+    random.seed(seed_init)
+    
+    scaler = StandardScaler()
+    X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype = float))
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    
+    n_classes = len(np.unique(y_train))
+    
+    # Try GMMs using different types of covariances.
+    estimator = GaussianMixture(n_components=n_classes,
+                  covariance_type='full', max_iter=20, random_state=0)
+    
+    #Spherical = variance is the same along all axes, which is not true;
+    #Tied = All gaussians share the same covariance matrix, which is not true;
+    #Diagonal = Assumes the features are independent from each other, may be interesting
+    #Full = Everyone has it's own covariance matrix
 
-n_classes = len(np.unique(y_train))
 
-# Try GMMs using different types of covariances.
-estimators = {cov_type: GaussianMixture(n_components=n_classes,
-              covariance_type=cov_type, max_iter=20, random_state=0)
-              for cov_type in ['full']}
-
-#Spherical = variance is the same along all axes, which is not true;
-#Tied = All gaussians share the same covariance matrix, which is not true;
-#Diagonal = Assumes the features are independent from each other, may be interesting
-#Full = Everyone has it's own covariance matrix
-
-n_estimators = len(estimators)
-
-for index, (name, estimator) in enumerate(estimators.items()):
     # Since we have class labels for the training data, we can
     # initialize the GMM parameters in a supervised manner.
     estimator.means_init = np.array([X_train[y_train == i].mean(axis=0)
@@ -85,18 +77,29 @@ for index, (name, estimator) in enumerate(estimators.items()):
     y_total = estimator.predict(X)
     
     # To visualize the result, we have a confusion matrix
-    conf_mat_total = confusion_matrix(y_total,y)
+    conf_mat_total.append(confusion_matrix(y_total,y))
     
-    # We calculate the accuracy using the diagonal of the confusion matrix (the songs that were correctly classified)
-    accuracy_total_prediction = 100*np.trace(conf_mat_total)/np.size(data,0)
     
-    # We save the confusion matrix as an CSV output
-    output = pd.DataFrame(conf_mat_total)
-    output = output.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='columns')
-    output = output.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='index')
-    
-    output.to_csv (r'../confusion_matrices/conf_matrix_GMM.csv', index = True, header=True)
-    
-    # Print the result to visualize in Spyder environement
-    print(output)
-    print("Total accuracy = ", accuracy_total_prediction, "%")
+# Calculate the mean and the variance of the confusion matrices
+conf_mat_total_mean = np.mean(conf_mat_total,axis=0)
+conf_mat_total_var = np.var(conf_mat_total,axis=0)
+
+# We calculate the accuracy using the diagonal of the confusion matrix (the songs that were correctly classified)
+accuracy_total_prediction = 100*np.trace(conf_mat_total_mean)/np.size(data,0)
+
+# We save the confusion matrix as an CSV output
+output_mean = pd.DataFrame(conf_mat_total_mean)
+output_mean = output_mean.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='columns')
+output_mean = output_mean.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='index')
+
+output_mean.to_csv (r'../confusion_matrices/conf_matrix_mean_GMM.csv', index = True, header=True)
+
+output_var = pd.DataFrame(conf_mat_total_var)
+output_var = output_var.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='columns')
+output_var = output_var.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='index')
+
+output_var.to_csv (r'../confusion_matrices/conf_matrix_var_GMM.csv', index = True, header=True)
+
+# Print the result to visualize in Spyder environement
+print(output_mean)
+print("Total accuracy = ", accuracy_total_prediction, "%")
