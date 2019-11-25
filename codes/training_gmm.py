f83b6ccd15327c7cb43d10c176f9787b007e9c1e
AlixYAN <56817634+AlixYAN@users.noreply.github.com>
2019-11-25 11:07:19 +0100

    Add files via upload

---
+++ codes/training_gmm.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 14 10:03:55 2019
+@author: Pedro, Rodrigo, Alix
+"""
+
+import librosa
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+# matplotlib inline
+import os
+from PIL import Image
+import pathlib
+import csv
+
+from sklearn import datasets
+from sklearn.externals.six.moves import xrange
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.mixture import GaussianMixture
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import confusion_matrix
+
+
+import warnings
+warnings.filterwarnings('ignore')
+
+#cmap = plt.get_cmap('inferno')
+
+data = pd.read_csv('./data.csv')
+data.head()
+            
+#data.shape
+
+# Dropping unneccesary columns
+data = data.drop(['filename'],axis=1)
+data = data.drop(['chroma_stft'],axis=1)
+data = data.drop(['rmse'],axis=1)
+data = data.drop(['spectral_centroid'],axis=1)
+data = data.drop(['spectral_bandwidth'],axis=1)
+data = data.drop(['rolloff'],axis=1)
+data = data.drop(['zero_crossing_rate'],axis=1)
+data = data.drop(['tempo'],axis=1)
+data = data.drop(['utempo'],axis=1)
+
+genre_list = data.iloc[:, -1]
+encoder = LabelEncoder()
+y = encoder.fit_transform(genre_list)
+
+scaler = StandardScaler()
+X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype = float))
+    
+for nb_iter in range(10) :
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    
+    taille = (0.2*np.size(data,0))
+    
+    #x_val = X_train[:200]
+    #partial_x_train = X_train[200:]
+    #
+    #y_val = y_train[:200]
+    #partial_y_train = y_train[200:]
+    
+    #colors = ['navy', 'turquoise', 'darkorange', 'red', 'black', 'green', 'yellow', 'purple', 'brown', 'blue']
+    colors = ['red', 'blue', 'green', 'yellow', 'orange']
+    
+    def make_ellipses(gmm, ax):
+        for n, color in enumerate(colors):
+            if gmm.covariance_type == 'full':
+                covariances = gmm.covariances_[n][:2, :2]
+            elif gmm.covariance_type == 'tied':
+                covariances = gmm.covariances_[:2, :2]
+            elif gmm.covariance_type == 'diag':
+                covariances = np.diag(gmm.covariances_[n][:2])
+            elif gmm.covariance_type == 'spherical':
+                covariances = np.eye(gmm.means_.shape[1]) * gmm.covariances_[n]
+            v, w = np.linalg.eigh(covariances)
+            u = w[0] / np.linalg.norm(w[0])
+            angle = np.arctan2(u[1], u[0])
+            angle = 180 * angle / np.pi  # convert to degrees
+            v = 2. * np.sqrt(2.) * np.sqrt(v)
+            ell = mpl.patches.Ellipse(gmm.means_[n, :2], v[0], v[1],
+                                      180 + angle, color=color)
+            ell.set_clip_box(ax.bbox)
+            ell.set_alpha(0.5)
+            ax.add_artist(ell)
+            ax.set_aspect('equal', 'datalim')
+    
+    n_classes = len(np.unique(y_train))
+    
+    # Try GMMs using different types of covariances.
+    estimators = {cov_type: GaussianMixture(n_components=n_classes,
+                  covariance_type=cov_type, max_iter=100, random_state=0)
+                  for cov_type in ['diag', 'full']}
+    
+    n_estimators = len(estimators)
+    
+    plt.figure(figsize=(3 * n_estimators // 2, 6))
+    plt.subplots_adjust(bottom=.01, top=0.95, hspace=.15, wspace=.05,
+                        left=.01, right=.99)
+    
+    
+    for index, (name, estimator) in enumerate(estimators.items()):
+        # Since we have class labels for the training data, we can
+        # initialize the GMM parameters in a supervised manner.
+        estimator.means_init = np.array([X_train[y_train == i].mean(axis=0)
+                                        for i in range(n_classes)])
+    
+        # Train the other parameters using the EM algorithm.
+        estimator.fit(X_train)
+    
+        h = plt.subplot(2, n_estimators // 2, index + 1)
+        make_ellipses(estimator, h)
+    
+        genres = np.unique(genre_list)
+    
+        for n, color in enumerate(colors):
+            data = X[y == n]
+            plt.scatter(data[:, 0], data[:, 1], s=0.8, color=color,
+                        label=genres[n])
+        # Plot the test data with crosses
+        for n, color in enumerate(colors):
+            data = X_test[y_test == n]
+            plt.scatter(data[:, 0], data[:, 1], marker='x', color=color)
+    
+        y_train_pred = estimator.predict(X_train)
+        train_accuracy = np.mean(y_train_pred.ravel() == y_train.ravel()) * 100
+        plt.text(0.05, 0.9, 'Train accuracy: %.1f' % train_accuracy,
+                 transform=h.transAxes)
+    
+        y_test_pred = estimator.predict(X_test)
+        test_accuracy = np.mean(y_test_pred.ravel() == y_test.ravel()) * 100
+        plt.text(0.05, 0.8, 'Test accuracy: %.1f' % test_accuracy,
+                 transform=h.transAxes)
+    
+        plt.xticks(())
+        plt.yticks(())
+        plt.title(name)
+        
+        y_tot = estimator.predict(X)
+    
+        
+        conf_mat = confusion_matrix(y_tot,y)
+        # 0.2 depends on dataset : test_size variable
+    
+        if nb_iter == 0:
+            conf_mat_total = conf_mat 
+        else:
+            for j in range(4):
+                for k in range(4):
+                     conf_mat_total[j][k] = (conf_mat_total[j][k]*i + conf_mat[j][k])/(i+1)
+        
+        total_acc = np.trace(conf_mat_total)
+            
+        output = pd.DataFrame(conf_mat_total)
+        output = output.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='columns')
+        output = output.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='index')
+    
+    
+        print("Confusion Matrix with cov =", estimator.covariance_type ,"in %\n", conf_mat)
+    
+        total_acc = 100*np.trace(conf_mat)/taille
+    
+        print("Test accuracy with cov =", estimator.covariance_type ,"\n" , total_acc , "%")
+    
+    plt.legend(scatterpoints=1, loc='lower right', prop=dict(size=12))
+    
+    
+    plt.show()
+
--- codes/training_nn.py
+++ codes/training_nn.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Oct 14 10:03:55 2019
-
 @author: Pedro, Rodrigo, Alix
 """
 
@@ -19,7 +18,7 @@ import csv
 # Preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-
+from sklearn.metrics import confusion_matrix
 # Keras
 from keras import models
 from keras import layers
@@ -29,92 +28,147 @@ warnings.filterwarnings('ignore')
 
 cmap = plt.get_cmap('inferno')
 
-data = pd.read_csv('../data.csv')
+data = pd.read_csv('./data.csv')
 data.head()
             
+labels = np.unique(data['label'])
+
 #data.shape
 
 # Dropping unneccesary columns
 data = data.drop(['filename'],axis=1)
+#data = data.drop(['chroma_stft'],axis=1)
+data = data.drop(['rmse'],axis=1)
+#data = data.drop(['spectral_centroid'],axis=1)
+data = data.drop(['spectral_bandwidth'],axis=1)
+data = data.drop(['rolloff'],axis=1)
+data = data.drop(['zero_crossing_rate'],axis=1)
+data = data.drop(['tempo'],axis=1)
+data = data.drop(['utempo'],axis=1)
+
+#{np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)} {np.mean(tempo)} {np.mean(utempo)}'
 
 genre_list = data.iloc[:, -1]
 encoder = LabelEncoder()
 y = encoder.fit_transform(genre_list)
 
-scaler = StandardScaler()
-X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype = float))
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+for i in range(10) :
 
-# len(y_train)
+    scaler = StandardScaler()
+    X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype = float))
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    
+    # len(y_train)
+    
+    # len(y_test)
+    
+    # X_train[10]
+    
+    #model = models.Sequential()
+    #model.add(layers.Dense(256, activation='relu', input_shape=(X_train.shape[1],)))
+    #
+    #model.add(layers.Dense(128, activation='relu'))
+    #
+    #model.add(layers.Dense(64, activation='relu'))
+    #
+    #model.add(layers.Dense(10, activation='softmax'))
+    #
+    #model.compile(optimizer='adam',
+    #              loss='sparse_categorical_crossentropy',
+    #              metrics=['accuracy'])
+    #
+    #history = model.fit(X_train,
+    #                    y_train,
+    #                    epochs=20,
+    #                    batch_size=128)
+    #
+    #test_loss, test_acc = model.evaluate(X_test,y_test)
+    #
+    #print('test_acc: ',test_acc)
+    
+    x_val = X_train[:200]
+    partial_x_train = X_train[200:]
+    
+    y_val = y_train[:200]
+    partial_y_train = y_train[200:]
+    
+    model = models.Sequential()
+    model.add(layers.Dense(512, activation='relu', input_shape=(X_train.shape[1],)))
+    model.add(layers.Dense(256, activation='relu'))
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(10, activation='softmax'))
+    
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    
+    history = model.fit(partial_x_train,
+                        partial_y_train,
+                        epochs=50,
+                        batch_size=512,
+                        validation_data=(x_val, y_val))
+    
+    results = model.evaluate(X_test, y_test)
+    
+    # Plot training & validation accuracy values
+    #plt.plot(history.history['acc'])
+    #plt.plot(history.history['val_acc'])
+    #plt.title('Model accuracy')
+    #plt.ylabel('Accuracy')
+    #plt.xlabel('Epoch')
+    #plt.legend(['Train', 'Test'], loc='upper left')
+    #plt.show()
+    
+    # Plot training & validation loss values
+    #plt.plot(history.history['loss'])
+    #plt.plot(history.history['val_loss'])
+    #plt.title('Model loss')
+    #plt.ylabel('Loss')
+    #plt.xlabel('Epoch')
+    #plt.legend(['Train', 'Test'], loc='upper left')
+    #plt.show()
+    
+    
+    y_total = model.predict(X)
+    
+    y_total = np.argmax(y_total,axis=1)
+    
+    conf_mat = confusion_matrix(y_total,y)
+    
+    if i == 0:
+        conf_mat_total = conf_mat 
+    else:
+        for j in range(4):
+            for k in range(4):
+                 conf_mat_total[j][k] = float(conf_mat_total[j][k]*i + conf_mat[j][k])/float(i+1)
+    
+    total_acc = np.trace(conf_mat_total)
+        
+    output = pd.DataFrame(conf_mat_total)
+    output = output.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='columns')
+    output = output.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='index')
+    
+    export_csv = output.to_csv (r'../conf_matrix_FFNN.csv', index = True, header=True)
 
-# len(y_test)
 
-# X_train[10]
+    for clabel in range(5) :
+        plt.plot(data['chroma_stft'][clabel*100:99+clabel*100], data['spectral_centroid'][clabel*100:99+clabel*100], 'x')
+        plt.ylabel('Spec Cent')
+        plt.xlabel('Chroma STFT')
+    plt.legend(['Classical', 'HipHop', 'Jazz', 'Reggae', 'Rock'], loc='upper left')
+    plt.show()
 
-#model = models.Sequential()
-#model.add(layers.Dense(256, activation='relu', input_shape=(X_train.shape[1],)))
+
+#predictions = model.predict(X_test)
 #
-#model.add(layers.Dense(128, activation='relu'))
+#print(predictions[0].shape)
 #
-#model.add(layers.Dense(64, activation='relu'))
+#print(np.sum(predictions[0]))
 #
-#model.add(layers.Dense(10, activation='softmax'))
-#
-#model.compile(optimizer='adam',
-#              loss='sparse_categorical_crossentropy',
-#              metrics=['accuracy'])
-#
-#history = model.fit(X_train,
-#                    y_train,
-#                    epochs=20,
-#                    batch_size=128)
-#
-#test_loss, test_acc = model.evaluate(X_test,y_test)
-#
-#print('test_acc: ',test_acc)
-
-x_val = X_train[:200]
-partial_x_train = X_train[200:]
-
-y_val = y_train[:200]
-partial_y_train = y_train[200:]
-
-model = models.Sequential()
-model.add(layers.Dense(512, activation='relu', input_shape=(X_train.shape[1],)))
-model.add(layers.Dense(256, activation='relu'))
-model.add(layers.Dense(128, activation='relu'))
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
-
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-
-history = model.fit(partial_x_train,
-                    partial_y_train,
-                    epochs=30,
-                    batch_size=512,
-                    validation_data=(x_val, y_val))
-results = model.evaluate(X_test, y_test)
-
-# Plot training & validation accuracy values
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
-
-# Plot training & validation loss values
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
+#print(np.argmax(predictions[0]))
 
 #predictions = model.predict(X_test)
 #
