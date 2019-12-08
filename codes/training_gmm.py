dc82dabbfaf9ea9c5af6976ff1a1df3fe444bc9f
Pedro Coutinho <pedro_coutinho01@hotmail.com>
2019-12-08 16:52:17 +0100

    Implementation of cross-correlation technique

--- codes/training_gmm.py
+++ codes/training_gmm.py
@@ -11,7 +11,7 @@ import numpy as np
 import random
 # matplotlib inline
 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.mixture import GaussianMixture
 from sklearn.metrics import confusion_matrix
@@ -37,6 +37,7 @@ scaler = StandardScaler()
 X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype = float))
 
 conf_mat_total = []
+conf_mat_test_total = []
 
 for seed_init in range(100):
 
@@ -44,57 +45,66 @@ for seed_init in range(100):
     np.random.seed(seed_init)
     random.seed(seed_init)
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    sss = StratifiedShuffleSplit(n_splits=5, test_size=0.2)
     
-    n_classes = len(np.unique(y_train))
+    conf_mat_test = []
     
-    # Try GMMs using different types of covariances.
-    estimator = GaussianMixture(n_components=n_classes,
-                  covariance_type='full', max_iter=20, random_state=0)
+    for train_index, test_index in sss.split(X, y):
     
-    #Spherical = variance is the same along all axes, which is not true;
-    #Tied = All gaussians share the same covariance matrix, which is not true;
-    #Diagonal = Assumes the features are independent from each other, may be interesting
-    #Full = Everyone has it's own covariance matrix
-
-
-    # Since we have class labels for the training data, we can
-    # initialize the GMM parameters in a supervised manner.
-    estimator.means_init = np.array([X_train[y_train == i].mean(axis=0)
-                                    for i in range(n_classes)])
-
-    # Train the other parameters using the EM algorithm.
-    estimator.fit(X_train)
-
-    y_train_pred = estimator.predict(X_train)
-    accuracy_train = np.mean(y_train_pred.ravel() == y_train.ravel()) * 100
-
-    y_test_pred = estimator.predict(X_test)
-    accuracy_test = np.mean(y_test_pred.ravel() == y_test.ravel()) * 100   
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        n_classes = len(np.unique(y_train))
+    
+        # Try GMMs using different types of covariances.
+        estimator = GaussianMixture(n_components=n_classes,
+                      covariance_type='full', max_iter=20, random_state=0)
+        
+        #Spherical = variance is the same along all axes, which is not true;
+        #Tied = All gaussians share the same covariance matrix, which is not true;
+        #Diagonal = Assumes the features are independent from each other, may be interesting
+        #Full = Everyone has it's own covariance matrix
     
     
-    # We apply now the trained model to the whole data to see how it behaves
-    y_total = estimator.predict(X)
+        # Since we have class labels for the training data, we can
+        # initialize the GMM parameters in a supervised manner.
+        estimator.means_init = np.array([X_train[y_train == i].mean(axis=0)
+                                        for i in range(n_classes)])
     
-    # To visualize the result, we have a confusion matrix
-    conf_mat_total.append(confusion_matrix(y_total,y))
+        # Train the other parameters using the EM algorithm.
+        estimator.fit(X_train)
     
+        y_train_pred = estimator.predict(X_train)
+        accuracy_train = np.mean(y_train_pred.ravel() == y_train.ravel()) * 100
+    
+        y_test_pred = estimator.predict(X_test)
+        accuracy_test = np.mean(y_test_pred.ravel() == y_test.ravel()) * 100   
+        
+        
+        # We apply now the trained model to the whole data to see how it behaves
+        y_total = estimator.predict(X)
+        
+        
+        # To visualize the result, we have a confusion matrix
+        #conf_mat_total.append(confusion_matrix(y_total,y))
+        conf_mat_test.append(confusion_matrix(y_test_pred,y_test))
+    
+    conf_mat_test_total.append(np.sum(conf_mat_test,axis=0))
     
 # Calculate the mean and the variance of the confusion matrices
-conf_mat_total_mean = np.mean(conf_mat_total,axis=0)
-conf_mat_total_var = np.var(conf_mat_total,axis=0)
+conf_mat_test_total_mean = np.mean(conf_mat_test_total,axis=0)
+conf_mat_test_total_var = np.var(conf_mat_test_total,axis=0)
 
 # We calculate the accuracy using the diagonal of the confusion matrix (the songs that were correctly classified)
-accuracy_total_prediction = 100*np.trace(conf_mat_total_mean)/np.size(data,0)
+accuracy_test_total_prediction = 100*np.trace(conf_mat_test_total_mean)/np.size(data,0)
 
 # We save the confusion matrix as an CSV output
-output_mean = pd.DataFrame(conf_mat_total_mean)
+output_mean = pd.DataFrame(conf_mat_test_total_mean)
 output_mean = output_mean.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='columns')
 output_mean = output_mean.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='index')
 
 output_mean.to_csv (r'../confusion_matrices/conf_matrix_mean_GMM.csv', index = True, header=True)
 
-output_var = pd.DataFrame(conf_mat_total_var)
+output_var = pd.DataFrame(conf_mat_test_total_var)
 output_var = output_var.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='columns')
 output_var = output_var.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='index')
 
@@ -102,4 +112,4 @@ output_var.to_csv (r'../confusion_matrices/conf_matrix_var_GMM.csv', index = Tru
 
 # Print the result to visualize in Spyder environement
 print(output_mean)
-print("Total accuracy = ", accuracy_total_prediction, "%")
+print("Total accuracy = ", accuracy_test_total_prediction, "%")
--- codes/training_nn.py
+++ codes/training_nn.py
@@ -14,7 +14,7 @@ import tensorflow as tf
 import random
 
 # Preprocessing
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.metrics import confusion_matrix
 
@@ -45,6 +45,7 @@ scaler = StandardScaler()
 X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype = float))
 
 conf_mat_total = []
+conf_mat_test_total = []
 
 #Seeds Initialization to reproduce results!
 for seed_init in range(100):
@@ -53,79 +54,86 @@ for seed_init in range(100):
     np.random.seed(seed_init)
     random.seed(seed_init)
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    sss = StratifiedShuffleSplit(n_splits=5, test_size=0.2)
     
-    x_val = X_train[:int(0.2*np.size(data,0))]
-    partial_x_train = X_train[int(0.2*np.size(data,0)):]
+    conf_mat_test = []
     
-    y_val = y_train[:int(0.2*np.size(data,0))]
-    partial_y_train = y_train[int(0.2*np.size(data,0)):]
+    for train_index, test_index in sss.split(X, y):
     
-    n_classes = len(np.unique(y_train))
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        n_classes = len(np.unique(y_train))
     
-    model = models.Sequential()
-    model.add(layers.Dense(512, activation='relu', input_shape=(X_train.shape[1],)))
-    #model.add(layers.Dense(256, activation='relu'))
-    #model.add(layers.Dense(128, activation='relu'))
-    #model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(n_classes, activation='softmax'))
+        model = models.Sequential()
+        model.add(layers.Dense(512, activation='relu', input_shape=(X_train.shape[1],)))
+        #model.add(layers.Dense(256, activation='relu'))
+        #model.add(layers.Dense(128, activation='relu'))
+        #model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(n_classes, activation='softmax'))
+        
+        opt = optimizers.Adam(lr = 0.0001, beta_1 = 0.9, beta_2 = 0.999, amsgrad = False)
+        
+        model.compile(optimizer=opt,
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+        
+        history = model.fit(X_train,
+                            y_train,
+                            epochs=20,
+                            batch_size=32,
+                            validation_data=(X_test, y_test))
+        
+        results = model.evaluate(X_test, y_test)
     
-    opt = optimizers.Adam(lr = 0.0001, beta_1 = 0.9, beta_2 = 0.999, amsgrad = False)
+    #    # Plot training & validation accuracy values
+    #    plt.plot(history.history['acc'])
+    #    plt.plot(history.history['val_acc'])
+    #    plt.title('Model accuracy')
+    #    plt.ylabel('Accuracy')
+    #    plt.xlabel('Epoch')
+    #    plt.legend(['Train', 'Test'], loc='upper left')
+    #    plt.show()
+    #    
+    #    # Plot training & validation loss values
+    #    plt.plot(history.history['loss'])
+    #    plt.plot(history.history['val_loss'])
+    #    plt.title('Model loss')
+    #    plt.ylabel('Loss')
+    #    plt.xlabel('Epoch')
+    #    plt.legend(['Train', 'Test'], loc='upper left')
+    #    plt.show()
     
-    model.compile(optimizer=opt,
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
     
-    history = model.fit(partial_x_train,
-                        partial_y_train,
-                        epochs=20,
-                        batch_size=32,
-                        validation_data=(x_val, y_val))
+#        y_total = model.predict(X)
+#        
+#        y_total = np.argmax(y_total,axis=1)
+#        
+#        conf_mat_total.append(confusion_matrix(y_total,y))
     
-    results = model.evaluate(X_test, y_test)
-
-#    # Plot training & validation accuracy values
-#    plt.plot(history.history['acc'])
-#    plt.plot(history.history['val_acc'])
-#    plt.title('Model accuracy')
-#    plt.ylabel('Accuracy')
-#    plt.xlabel('Epoch')
-#    plt.legend(['Train', 'Test'], loc='upper left')
-#    plt.show()
-#    
-#    # Plot training & validation loss values
-#    plt.plot(history.history['loss'])
-#    plt.plot(history.history['val_loss'])
-#    plt.title('Model loss')
-#    plt.ylabel('Loss')
-#    plt.xlabel('Epoch')
-#    plt.legend(['Train', 'Test'], loc='upper left')
-#    plt.show()
-
-
-    y_total = model.predict(X)
+        y_test_pred = model.predict(X_test)
+        
+        y_test_pred = np.argmax(y_test_pred,axis=1)
+        
+        conf_mat_test.append(confusion_matrix(y_test_pred,y_test))
     
-    y_total = np.argmax(y_total,axis=1)
-    
-    conf_mat_total.append(confusion_matrix(y_total,y))
-    
+    conf_mat_test_total.append(np.sum(conf_mat_test,axis=0))
     
     
 # Calculate the mean and the variance of the confusion matrices
-conf_mat_total_mean = np.mean(conf_mat_total,axis=0)
-conf_mat_total_var = np.var(conf_mat_total,axis=0)
+conf_mat_test_total_mean = np.mean(conf_mat_test_total,axis=0)
+conf_mat_test_total_var = np.var(conf_mat_test_total,axis=0)
 
 # We calculate the accuracy using the diagonal of the confusion matrix (the songs that were correctly classified)
-accuracy_total_prediction = 100*np.trace(conf_mat_total_mean)/np.size(data,0)
+accuracy_test_total_prediction = 100*np.trace(conf_mat_test_total_mean)/np.size(data,0)
 
 # We save the confusion matrix as an CSV output
-output_mean = pd.DataFrame(conf_mat_total_mean)
+output_mean = pd.DataFrame(conf_mat_test_total_mean)
 output_mean = output_mean.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='columns')
 output_mean = output_mean.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='index')
 
 output_mean.to_csv (r'../confusion_matrices/conf_matrix_mean_GMM.csv', index = True, header=True)
 
-output_var = pd.DataFrame(conf_mat_total_var)
+output_var = pd.DataFrame(conf_mat_test_total_var)
 output_var = output_var.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='columns')
 output_var = output_var.rename({idx:labels[idx] for idx in range(np.size(labels))},axis='index')
 
@@ -133,7 +141,7 @@ output_var.to_csv (r'../confusion_matrices/conf_matrix_var_GMM.csv', index = Tru
 
 # Print the result to visualize in Spyder environement
 print(output_mean)
-print("Total accuracy = ", accuracy_total_prediction, "%")
+print("Total accuracy = ", accuracy_test_total_prediction, "%")
 
 #predictions = model.predict(X_test)
 #
